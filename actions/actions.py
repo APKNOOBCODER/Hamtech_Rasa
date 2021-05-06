c5df32ec285c3099f89c308c20536806911e0f7d
APKNOOBCODER <amirpk.d501@gmail.com>
2021-05-06 17:45:18 +0430

    log001

--- actions/actions.py
+++ actions/actions.py
@@ -534,7 +534,8 @@ class ActionDrugInterferences2(Action):
                 for name in data:
                     if name == drug_name_1 or drug_name_1 in name:
                         Drug_Interferences = data[name]["Cautions"]["Drug_Interferences"]
-                        print(Drug_Interferences)
+                        # print(Drug_Interferences)
+                        print(drug_name_2 in Drug_Interferences)
                         if drug_name_2 in Drug_Interferences:
                             ans = "بلی. دارو ها با هم تداخل دارند: \n %s" % Drug_Interferences
                             print("found")
