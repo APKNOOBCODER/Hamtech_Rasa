8b83c962cd154a4856a03eb3d3c788280cc55ceb
APKNOOBCODER <amirpk.d501@gmail.com>
2021-06-14 23:00:26 +0430

    last changes for newly added sentences

--- data/random_entity.py
+++ data/random_entity.py
@@ -1,20 +1,31 @@
 import random
 import os
 import json
+import yaml
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-f = open(dir_path + "/../actions/data.json", "r", encoding='utf-8')
-data:dict = json.loads(f.read())
-
-drug_names = []
+with open(dir_path + "/nlu.yml", "r", encoding='utf-8') as f:
+    nluyml:dict = yaml.full_load(f)
+    drugL:str = nluyml["nlu"][21]["examples"]
+    symptomL:str = nluyml["nlu"][22]["examples"]
+    illnessL:str = nluyml["nlu"][23]["examples"]
+    drugList:list = drugL[2:-1].split("\n- ")
+    symptomList:list = symptomL[2:-1].split("\n- ")
+    illnessList:list = illnessL[2:-1].split("\n- ")
+    # print(drugList)
+"""
 
 for name in data:
     drug_names.append(name)
-
+"""
 n = int(input("how many drugs needed?!"))
 for x in range(n):
-    index1 = random.randrange(0,len(drug_names))
-    index2 = random.randrange(0,len(drug_names))
+    dindex1 = random.randrange(0,len(drugList))
+    dindex2 = random.randrange(0,len(drugList))
+    sindex = random.randrange(0,len(symptomList))
+    iindex = random.randrange(0,len(illnessList))
     # "    - ["+ drug_names[index1] +"](drug_name) و ["+ drug_names[index2] +"](drug_name) مثل همن؟"
-    print("    - آیا ["+ drug_names[index1] +"](drug_name) برای ["+ drug_names[index2] +"](drug_name) بده؟")
-
+    # [" + drugList[dindex1] + "](drug_name)
+    # print(symptomList[sindex])
+    print("    - چه دارو هایی رو نباید با [" + drugList[dindex1] + "](drug_name) بخورم؟")
